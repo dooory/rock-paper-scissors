fa34f6c56a95885fe7e03947407a9c76afa50acc
dooory <78720364+dooory@users.noreply.github.com>
2025-06-09 14:52:35 +1200

    Initialized Repository
    I forgot about creating a repository for the project

---
+++ script.js
@@ -0,0 +1,32 @@
+// Creating a rock paper scissors program
+//
+// Create a constant variable for an array containing the strings, rock, paper, and scissors.
+const answers = ["rock", "paper", "scissors"];
+// Create a function to generate a random number between the parameters min and max
+function generateRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Using the generateRandomInt function, generate a number between 0-2 and store it in a variable named randomAnswerIndex
+let randomAnswerIndex = generateRandomInt(0, 2);
+
+let randomAnswer = answers[randomAnswerIndex];
+
+console.log(randomAnswer);
+// Ask the user which answer they pick and store it in a let variable.
+// 
+// Print the results to the user.
+//
+// If both answers are equal, repeat the last 3 steps until not true
+//
+// If user picked rock,
+//      and the computer picked scissors the user wins.
+//      If the computer picked paper the user loses.
+// If user picked paper,
+//      and the computer picked rock the user wins.
+//      If the computer picked scissors the user loses.
+// If user picked scissors,
+//      and the computer picked paper the user wins.
+//      If the computer picked rock the user loses
+// 
+// Ask the user if they want to play again
