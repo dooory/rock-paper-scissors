7f8e1f21bcbd232c8e1310ebb2244bb321d5bb0f
dooory <78720364+dooory@users.noreply.github.com>
2025-06-18 14:52:38 +1200

    feat: weapons buttons act as playround buttons

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const computerScore = document.querySelector("#computerScore");
 // For looping through and adding click event listeners
 const weaponButtons = document.querySelectorAll(".weaponButton");
 
-const weaponsArray = ["rock", "paper", "scissors"];
+const weaponsArray = ["Rock", "Paper", "Scissors"];
 
 // generates a random number inclusively, so it can be >= min and <= max
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -15,7 +15,20 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
-// used to decide the computers weapon of choice
+// decides the computers weapon of choice
 function getRandomWeapon() {
-    return weaponsArray[getRandomNumber(0, weaponsArray.length - 1)]
+    return weaponsArray[getRandomNumber(0, weaponsArray.length - 1)];
 }
+
+function playRound(userWeapon) {
+    const computerWeapon = getRandomWeapon();
+
+    console.log(userWeapon, computerWeapon);
+}
+
+weaponButtons.forEach(button => {
+   // sending through the name of the buttons weapon
+   button.addEventListener("click", element => {
+        playRound(element.target.textContent);
+   }) 
+});
