5020c522a5449c5d45c1b1ebb009935e3f7c3ab2
dooory <78720364+dooory@users.noreply.github.com>
2025-06-18 14:28:29 +1200

    Created element variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Test");
+const announcerText = document.querySelector("#announceText");
+const userScore = document.querySelector("#userScore");
+const computerScore = document.querySelector("#computerScore");
+
+const weaponButtons = document.querySelectorAll(".weaponButton");
