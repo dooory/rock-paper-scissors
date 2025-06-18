8501a1f88e05eb930e2e6c32c6ae2ac83076d886
dooory <78720364+dooory@users.noreply.github.com>
2025-06-18 14:40:54 +1200

    Added random weapon generator

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ const announcerText = document.querySelector("#announceText");
 const userScore = document.querySelector("#userScore");
 const computerScore = document.querySelector("#computerScore");
 
+// For looping through and adding click event listeners
 const weaponButtons = document.querySelectorAll(".weaponButton");
+
+const weaponsArray = ["rock", "paper", "scissors"];
+
+// generates a random number inclusively, so it can be >= min and <= max
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomNumber(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+// used to decide the computers weapon of choice
+function getRandomWeapon() {
+    return weaponsArray[getRandomNumber(0, weaponsArray.length - 1)]
+}
