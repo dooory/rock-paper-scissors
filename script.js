bb850b0f29b127741ca228a63bea01cb8e82de86
dooory <78720364+dooory@users.noreply.github.com>
2025-06-18 15:31:23 +1200

    feat: added score tallying + round winner algorithim + misc changes/fixes

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const announcerText = document.querySelector("#announceText");
-const userScore = document.querySelector("#userScore");
-const computerScore = document.querySelector("#computerScore");
+const announcerText = document.querySelector("#announcerText");
+
+const userWeaponElement = document.querySelector("#userWeapon");
+const userScoreElement = document.querySelector("#userScore");
+
+const computerWeaponElement = document.querySelector("#computerWeapon");
+const computerScoreElement = document.querySelector("#computerScore");
 
 // For looping through and adding click event listeners
 const weaponButtons = document.querySelectorAll(".weaponButton");
 
-const weaponsArray = ["Rock", "Paper", "Scissors"];
+const WEAPONS_ARRAY = ["Rock", "Paper", "Scissors"];
 
-// generates a random number inclusively, so it can be >= min and <= max
+let userScore = 0; 
+let computerScore = 0;
+
+// generates a random number inclusively, so it can be between >= min and <= max
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomNumber(min, max) {
   const minCeiled = Math.ceil(min);
@@ -17,15 +24,57 @@ function getRandomNumber(min, max) {
 
 // decides the computers weapon of choice
 function getRandomWeapon() {
-    return weaponsArray[getRandomNumber(0, weaponsArray.length - 1)];
+    return WEAPONS_ARRAY[getRandomNumber(0, WEAPONS_ARRAY.length - 1)];
+}
+
+function getRoundWinner(userWeapon, computerWeapon) {
+    // The if statements dont check if it was a tie or not
+    // So this variable will be sent as "No one" if it is a tie 
+    let winner = "No one";
+
+    // Winning order Paper > Rock > Scissors > Paper
+
+    if (userWeapon === "Paper" && computerWeapon === "Rock") {
+        winner = "User";
+    } else if (userWeapon === "Paper" && computerWeapon === "Scissors") {
+        winner = "Computer";
+    }
+
+    if (userWeapon === "Rock" && computerWeapon === "Scissors") {
+        winner = "User";
+    } else if (userWeapon === "Rock" && computerWeapon === "Paper") {
+        winner = "Computer";
+    }
+
+    if (userWeapon === "Scissors" && computerWeapon === "Paper") {
+        winner = "User";
+    } else if (userWeapon === "Scissors" && computerWeapon === "Rock") {
+        winner = "Computer";
+    }
+
+    return winner;
 }
 
 function playRound(userWeapon) {
     const computerWeapon = getRandomWeapon();
+    const roundWinner = getRoundWinner(userWeapon, computerWeapon); 
+    
+    userWeaponElement.textContent = `You chose ${userWeapon}`
+    computerWeaponElement.textContent = `The Computer chose ${computerWeapon}`
+    announcerText.textContent = `${roundWinner === "User" ? "You" : roundWinner} won that round!`
 
-    console.log(userWeapon, computerWeapon);
+    if (roundWinner === "User") {
+        userScore++;
+        userScoreElement.textContent = `Your score: ${userScore}`;
+    } else if (roundWinner === "Computer") {
+        computerScore++;
+        computerScoreElement.textContent = `Computer score: ${computerScore}`;
+    }
+
+    return roundWinner
 }
 
+// looping through each weapon's button
 weaponButtons.forEach(button => {
    // sending through the name of the buttons weapon
    button.addEventListener("click", element => {
